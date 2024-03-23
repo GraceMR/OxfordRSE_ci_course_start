c918183ae9b6a60008a5f1a178756c29af37d87b
Grace Ryall <grace.ryall@bristol.ac.uk>
2024-03-23 12:03:12 +0000

    build was failing in build the docs- may be because source.py is not in the source folder?

---
+++ docs/source.py
@@ -0,0 +1,19 @@
+#
+# For the full list of built-in configuration values, see the documentation:# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information --------------------------------------------------># https://www.sphinx-doc.org/en/master/usage/configuration.html#project-inf>
+project = 'oxfordrse_ci_course'
+copyright = '2024, Grace R'
+author = 'Grace R'
+
+# -- General configuration ------------------------------------------------># https://www.sphinx-doc.org/en/master/usage/configuration.html#general-con>
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output ----------------------------------------------># https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for>
+html_theme = 'alabaster'
+html_static_path = ['_static']
